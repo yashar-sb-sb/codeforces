6ad3ce4a806bbd624de0640f8c38c941b6d79be5
yashar-sb-sb <yashar_sb_sb@yahoo.com>
2016-02-23 10:37:04 +0330

    Update solved-problem-letters.py

--- statistical/solved-problem-letters.py
+++ statistical/solved-problem-letters.py
@@ -1,4 +1,11 @@
+import urllib.request
+import json
+
 def get(handle):
 '''
 fetches data from codeforces api and stores it into data/handles/handle/
 '''
+  sj = json.loads(urllib.request.urlopen('http://codeforces.com/api/user.status?handle=yashar_sb_sb&from=1').read().decode('utf-8'))
+  cj = json.loads(urllib.request.urlopen('http://codeforces.com/api/contest.list').read().decode('utf-8'))
+  
+  f = open('data/handles/'+handle+'/result','w')
