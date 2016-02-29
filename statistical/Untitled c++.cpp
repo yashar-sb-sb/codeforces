c02513b5a174657c1414f6f9484e1f9957db086d
yashar-sb-sb <yashar_sb_sb@yahoo.com>
2016-02-29 12:59:53 +0330

    adding a new file for teaching

---
+++ statistical/Untitled c++.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	cout<<"Hello\n";	
+	return 0;
+}
--- statistical/solved-problem-letters.py
+++ statistical/solved-problem-letters.py
@@ -1,3 +1,5 @@
+#this file is edited
+
 import urllib.request
 import json
 
